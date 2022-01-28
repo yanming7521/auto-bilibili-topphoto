692fcd5eaa8869bff37ea2334c61888d70eb98a2
57code <yt0379@sohu.com>
2022-01-28 17:56:32 +0800

    绘制粉丝头像

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-const { createCanvas, loadImage ,registerFont } = require('canvas');
+const { createCanvas, loadImage, registerFont } = require("canvas");
 const fs = require("fs");
-const axios = require('axios');
+const axios = require("axios");
 
-const [BILI_JCT, SESSDATA,DEDEUSERID,DEDEUSERID__CKMD5] = process.argv.slice(2);
-
+const [BILI_JCT, SESSDATA, DEDEUSERID, DEDEUSERID__CKMD5] =
+  process.argv.slice(2);
 //注册字体
-registerFont('digit.ttf', { family: 'digit' });
+registerFont("digit.ttf", { family: "digit" });
 
 //B站推荐头图尺寸2560x400，最小尺寸1280x200
-const canvasSize = {w:2560,h:400};
+const canvasSize = { w: 2560, h: 400 };
 
 //创建画布
 const canvas = createCanvas(canvasSize.w, canvasSize.h);
 //获得画布的绘制对象
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 //清空画布
-function clearCanvas(bgColor='#000000'){
-    ctx.fillStyle = bgColor;
-    ctx.fillRect(0,0,canvasSize.w, canvasSize.h);
+function clearCanvas(bgColor = "#000000") {
+  ctx.fillStyle = bgColor;
+  ctx.fillRect(0, 0, canvasSize.w, canvasSize.h);
 }
 
 //将画布保存为本地文件
-function saveCanvasToImage(file){
-    fs.writeFileSync(file,canvas.toBuffer());
+function saveCanvasToImage(file) {
+  fs.writeFileSync(file, canvas.toBuffer());
 }
 
 //上传B站头图
-async function postTopPhoto(base64){
-    const res = await axios.request({
-        url:"https://space.bilibili.com/ajax/topphoto/uploadTopPhotov2",
-        method:"POST",
-        data:"topphoto="+base64+"&csrf="+BILI_JCT,
-        headers:{
-            'referer': 'https://space.bilibili.com/'+DEDEUSERID,
-            "accept": "application/json, text/plain, */*",
-            "Content-Type":"application/x-www-form-urlencoded",
-            "Origin": "https://space.bilibili.com",
-            "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-            "Cookie":
-                "SESSDATA="+SESSDATA+";"+
-                "bili_jct="+BILI_JCT+";"+
-                "DedeUserID="+DEDEUSERID+";"+
-                "DedeUserID__ckMd5="+DEDEUSERID__CKMD5+";"
-        }
-    })
+async function postTopPhoto(base64) {
+  const res = await axios.request({
+    url: "https://space.bilibili.com/ajax/topphoto/uploadTopPhotov2",
+    method: "POST",
+    data: "topphoto=" + base64 + "&csrf=" + BILI_JCT,
+    headers: {
+      referer: "https://space.bilibili.com/" + DEDEUSERID,
+      accept: "application/json, text/plain, */*",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Origin: "https://space.bilibili.com",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+      Cookie:
+        "SESSDATA=" +
+        SESSDATA +
+        ";" +
+        "bili_jct=" +
+        BILI_JCT +
+        ";" +
+        "DedeUserID=" +
+        DEDEUSERID +
+        ";" +
+        "DedeUserID__ckMd5=" +
+        DEDEUSERID__CKMD5 +
+        ";",
+    },
+  });
 }
 
 /*
@@ -53,51 +62,118 @@ https://space.bilibili.com/api/member/uploadTopPhoto?access_key=8a135f48fc67e821
 */
 
 //计算两个日期相差的天数
-const diffDays = (date, otherDate) => Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+const diffDays = (date, otherDate) =>
+  Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+
+//获取当前粉丝数
+async function getFansCount() {
+  try {
+    const res = await axios.request({
+      url: "https://api.bilibili.com/x/web-interface/card?mid=" + DEDEUSERID,
+    });
+    return res.data.data.follower;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 //获取自己鸽的天数
-async function getLastPubDistance(){
-    //https://api.bilibili.com/x/space/arc/search?mid="+(event.bili_user_id||'422646817')+"&ps=10&tid=0&pn="+(event.page||1)+"&keyword=&order=pubdate&jsonp=jsonp
-    const res = await axios.request({
-        url:"https://api.bilibili.com/x/space/arc/search?mid="+DEDEUSERID+"&pn=1&ps=1&order=pubdate&jsonp=jsonp"
-    });
+async function getLastPubDistance() {
+  //https://api.bilibili.com/x/space/arc/search?mid="+(event.bili_user_id||'422646817')+"&ps=10&tid=0&pn="+(event.page||1)+"&keyword=&order=pubdate&jsonp=jsonp
+  const res = await axios.request({
+    url:
+      "https://api.bilibili.com/x/space/arc/search?mid=" +
+      DEDEUSERID +
+      "&pn=1&ps=1&order=pubdate&jsonp=jsonp",
+  });
 
-    
-    try{
-        return diffDays(new Date(res.data.data.list.vlist[0].created*1000),new Date())+"";
-    }catch(err){
-        console.log("没有发现投稿");
-        console.log(err);
-        return 0;
+  try {
+    return (
+      diffDays(
+        new Date(res.data.data.list.vlist[0].created * 1000),
+        new Date()
+      ) + ""
+    );
+  } catch (err) {
+    console.log("没有发现投稿");
+    console.log(err);
+    return 0;
+  }
+}
+
+async function getFansInfo() {
+  const order = "desc";
+  const orderType = "attention";
+  const url =
+    "http://api.bilibili.com/x/relation/followers?" +
+    "vmid=" +
+    DEDEUSERID +
+    "&order=" +
+    order +
+    "&order_type=" +
+    orderType;
+
+  let list = [];
+
+  for (let i = 1; i <= 5; i++) {
+    try {
+      const res = await axios.request(url + "&pn=" + i);
+      if (res.data.code === 0 && res.data.data.list) {
+        list = list.concat(res.data.data.list);
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    
+  }
+  return list;
 }
 
 //执行绘制图片
-async function painting(){
-    clearCanvas();
+async function painting() {
+  clearCanvas();
 
-    //添加背景图
-    const bgImage = await loadImage('bg.jpg');
-    ctx.drawImage(bgImage, 0, 0, canvasSize.w, canvasSize.h);
+  //添加背景图
+  const bgImage = await loadImage("bg.jpg");
+  ctx.drawImage(bgImage, 0, 0, canvasSize.w, canvasSize.h);
 
-    //设置文字颜色和字号，字体
-    ctx.fillStyle = "#e6433a";
-    ctx.font = '97px digit';
+  //获得粉丝数据
+  const infos = await getFansInfo();
+  let face;
+  // 2560 * 400 = x * 250
+  // x = 4096
+  const w = 64;
+  const col = 40,
+    row = 6;
+  let offsetX = 0,
+    offsetY = 0;
+  let n = 0;
+  for (let i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      face = await loadImage(infos[n++].face);
+      offsetX = w * i;
+      offsetY = w * j;
+      offsetX = ctx.drawImage(face, offsetX, offsetY, 64, 64);
+    }
+  }
 
-    //获得鸽的天数
-    let days = await getLastPubDistance();
+  //获得鸽的天数
+  //let days = await getLastPubDistance();
 
-    //计算文字尺寸
-    let size = ctx.measureText(days);
-    const txt_x = 2020;
-    const txt_y = 135;
+  //设置文字颜色和字号，字体
+  //   ctx.fillStyle = "#e6433a";
+  //   ctx.font = "72px digit";
 
-    //转换后续的transform的基点
-    ctx.translate(txt_x, txt_y);
+  //获得粉丝数
+  //   let count = await getFansCount();
+  //计算文字尺寸
+  //   let size = ctx.measureText(count);
+  //   const txt_x = 2020;
+  //   const txt_y = 135;
 
-    /*
+  //转换后续的transform的基点
+  //ctx.translate(txt_x, txt_y);
+
+  /*
     a	水平缩放绘图
     b	水平倾斜绘图
     c	垂直倾斜绘图
@@ -105,14 +181,17 @@ async function painting(){
     e	水平移动绘图
     f	垂直移动绘图
     */
-    ctx.transform(1,-0.3,0,1,0,0);
+  //ctx.transform(1, -0.4, 0, 1, 0, 0);
 
-    //将文字绘制到指定坐标
-    ctx.fillText(days, -size.width/2, 0);
+  //将文字绘制到指定坐标
+  //ctx.fillText(count, -size.width / 2, 0);
 
-    //canvas转base64编码后上传
-    const base64 = encodeURIComponent(canvas.toDataURL("image/png").substring(22));
-    postTopPhoto(base64);
+  //canvas转base64编码后上传
+  const base64 = encodeURIComponent(
+    canvas.toDataURL("image/png").substring(22)
+  );
+  postTopPhoto(base64);
+  // saveCanvasToImage("./bg_preview.jpg");
 }
 
 painting();
